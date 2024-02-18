5a894cfe0a29a41e2c7254c0d74596424bc3f630
SiddaharthaPaudel <psiddhartha62@gmail.com>
2024-02-18 22:46:59 +0545

    Adding input filled

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,11 @@ import 'firebase_options.dart';
 
 
 void main() async {
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
