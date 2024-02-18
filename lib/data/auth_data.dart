a6ba12da76923551ad1d46340875b41b0d4e8b5c
SiddaharthaPaudel <psiddhartha62@gmail.com>
2024-02-18 22:38:21 +0545

    Login Password Confirmation

--- lib/data/auth_data.dart
+++ lib/data/auth_data.dart
@@ -10,14 +10,18 @@ abstract class AuthenticationDatasource {
 
 class AuthenticationRemote extends AuthenticationDatasource {
   @override
-  Future<void> login(String email, String password) async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email.trim(), password: password.trim());
-  }
+  
 
   @override
   Future<void> register(
       String email, String password, String PasswordConfirm) async {
-   
+    if (PasswordConfirm == password) {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+          email: email.trim(), password: password.trim())
+          .then((value) {
+        Firestore_Datasource().CreateUser(email);
+      });
+    }
   }
 }
